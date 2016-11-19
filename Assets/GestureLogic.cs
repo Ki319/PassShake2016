030d594b88e42b2651fe38e85f0ef881b7724c98
Jordan Buckmaster <buckmast@purdue.edu>
2016-11-19 13:22:22 -0600

    working on gesture comparing

--- Assets/GestureLogic.cs
+++ Assets/GestureLogic.cs
@@ -31,31 +31,39 @@ public class GestureLogic : MonoBehaviour {
 
     bool DetectChange(Hand curr)
     {
-        if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 5)
+        if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 20)
             return false;
-        else if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 5)
+        else if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 20)
             return false;
-        else if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 5)
+        else if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 20)
             return false;
         for(int i = 0; i < curr.Fingers.Count; i++)
         {
-            if(curr.Fingers[i].StabilizedTipPosition.x - holdHand.Fingers[i].StabilizedTipPosition.x >= 5)
+            if(curr.Fingers[i].StabilizedTipPosition.x - holdHand.Fingers[i].StabilizedTipPosition.x >= 20)
                 return false;
-            else if (curr.Fingers[i].StabilizedTipPosition.y - holdHand.Fingers[i].StabilizedTipPosition.y >= 5)
+            else if (curr.Fingers[i].StabilizedTipPosition.y - holdHand.Fingers[i].StabilizedTipPosition.y >= 20)
                 return false;
-            else if (curr.Fingers[i].StabilizedTipPosition.z - holdHand.Fingers[i].StabilizedTipPosition.z >= 5)
+            else if (curr.Fingers[i].StabilizedTipPosition.z - holdHand.Fingers[i].StabilizedTipPosition.z >= 20)
                 return false;
         }
         return true;
     }
 
-    //compares inputted gesture sequence to current set PassShake. Returns 1 for success, 0 for failure.
-    int checkPass()
+    //compares inputted gesture sequence to current set PassShake. Returns true for success, false for failure.
+    bool CheckPass()
     {
         if(gestures.Count - 1 == correct.Count) //including end gesture in sequence. checks to see if lengths are equal.
         {
+            for(int i = 0; i < correct.Count; i++)
+            {
+                for(int j = 0; j < gestures[i].Fingers.Count; j++)
+                {
+                    if ((gestures[i].Fingers[j].StabilizedTipPosition.x - gestures[i].PalmPosition.x) - (correct[i].Fingers[j].StabilizedTipPosition.x - correct[i].PalmPosition.x) >= 20)
+                        return false;
 
+                }
+            }
         }
-        return 0;
+        return false;
     }
 }
