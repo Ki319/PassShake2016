26cc2237f4764776338edac1efd57d2632f437f5
JordanR159 <reed226@purdue.edu>
2016-11-19 10:01:02 -0600

    Gesture logic

---
+++ Assets/GestureLogic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Unity;
+using System.Collections.Generic;
+
+public class GestureLogic : MonoBehaviour {
+
+    private List<Hand> gestures = new List<Hand>();
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
