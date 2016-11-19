18282cb92a815f0c69d306e0a0796a9aaaf06aa8
Aaron Neustedter <aaron97neu@yahoo.com>
2016-11-19 16:20:40 -0500

    New Scenes
    Added new Scenes with content

---
+++ Assets/Scene_Manager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
