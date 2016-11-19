7076903ece4268fd4e841739459667de9ac18f94
Jordan Buckmaster <buckmast@purdue.edu>
2016-11-19 10:58:21 -0600

    gesture changes

--- Assets/GestureLogic.cs
+++ Assets/GestureLogic.cs
@@ -6,15 +6,15 @@ using System.Collections.Generic;
 
 public class GestureLogic : MonoBehaviour {
 
-    private List<Hand> gestures = new List<Hand>(); //List of gesture so far
+    private List<Hand> gestures = new List<Hand>();//List of gesture so far
+    public List<Hand> correct = new List<Hand>(); //Correct passshake
     private float holdPositionTime;                      //Time that certain position is held
-    private static Hand holdHand;                        //Last state of hand before position hold
+    private static Hand holdHand;   //Last state of hand before position hold
+    public Hand endHand;
 
 	void Start () {
         CopyHand(HandRepresentation.MostRecentHand);
         holdPositionTime = Time.time;
-
-	
 	}
 	
 	// Update is called once per frame
@@ -25,5 +25,16 @@ public class GestureLogic : MonoBehaviour {
     void CopyHand(Hand curr)
     {
         holdHand = curr;
+    }
+
+
+    //compares inputted gesture sequence to current set PassShake. Returns 1 for success, 0 for failure.
+    int checkPass()
+    {
+        if(gestures.Count - 1 == correct.Count) //including end gesture in sequence. checks to see if lengths are equal.
+        {
+
+        }
+        return 0;
     }
 }
