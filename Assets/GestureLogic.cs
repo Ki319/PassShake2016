b739fb57f70171d731f172daa1ca7c59c1a465d8
JordanR159 <reed226@purdue.edu>
2016-11-19 10:58:25 -0600

    Handsel and Gretal

--- Assets/GestureLogic.cs
+++ Assets/GestureLogic.cs
@@ -11,12 +11,13 @@ public class GestureLogic : MonoBehaviour {
     private static Hand holdHand;                        //Last state of hand before position hold
 
 	void Start () {
-        CopyHand();
+        CopyHand(HandRepresentation.MostRecentHand);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(DetectChange(HandModel._ha)
 	
 	}
 
@@ -24,4 +25,25 @@ public class GestureLogic : MonoBehaviour {
     {
         holdHand = curr;
     }
+
+    bool DetectChange(Hand curr)
+    {
+        if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 5)
+            return false;
+        else if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 5)
+            return false;
+        else if (curr.PalmPosition.x - holdHand.PalmPosition.x >= 5)
+            return false;
+        for(int i = 0; i < curr.Fingers.Count; i++)
+        {
+            if(curr.Fingers[i].StabilizedTipPosition.x - holdHand.Fingers[i].StabilizedTipPosition.x >= 5)
+                return false;
+            else if (curr.Fingers[i].StabilizedTipPosition.y - holdHand.Fingers[i].StabilizedTipPosition.y >= 5)
+                return false;
+            else if (curr.Fingers[i].StabilizedTipPosition.z - holdHand.Fingers[i].StabilizedTipPosition.z >= 5)
+                return false;
+        }
+        return true;
+    }
+        
 }
