2a64574e883956828c7e05b26f98d46e5ad681b4
Jordan Buckmaster <buckmast@purdue.edu>
2016-11-19 13:50:59 -0600

    c

--- Assets/GestureLogic.cs
+++ Assets/GestureLogic.cs
@@ -22,10 +22,7 @@ public class GestureLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(DetectChange(hand.GetLeapHand())) {
-
-
-        }
+        if(DetectChange(HandModel._ha)
 	
 	}
 
