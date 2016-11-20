a7444dc02ce06c1a7fcdd392e9953276146868eb
Aaron Neustedter <aaron97neu@yahoo.com>
2016-11-19 21:55:04 -0500

    added show and hide methods to checkmark

---
+++ Assets/CheckmarkSprite.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckmarkSprite : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    SpriteRenderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void show(){
+        SpriteRenderer.enabled = true;
+    }
+    public void hide(){
+        SpriteRenderer.enabled = false;
+    }
+}
